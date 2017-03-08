bb7bd9e76e2f323300316aa57507f9c5455ddb94
sean-cpw <sean@cmykpluswhite.com>
2017-03-07 22:44:58 -0500

    Create email.php

---
+++ themes/throne-child/email.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Template Name: Newsletter Page
+ */
+?>
+<?php get_header(); ?>
+
+<section id="main" class="content_wrapper">
+
+	<div id="tfk_company_db_header">
+		<h1>Please Subscribe to Tech for Korea's Newsletter</h1>
+
+		<!-- Begin MailChimp Signup Form -->
+		<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
+		<style type="text/css">
+			#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+			/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+			   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+		</style>
+		<div id="mc_embed_signup">
+			<form action="//techforkorea.us10.list-manage.com/subscribe/post?u=37ab6470efbc61209ca1850ed&amp;id=c897d012ba" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+			    <div id="mc_embed_signup_scroll">
+					<h2>SIGN UP FOR OUR WEEKLY NEWSLETTERS</h2>
+					<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+					<div class="mc-field-group">
+						<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+						</label>
+						<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+					</div>
+					<div id="mce-responses" class="clear">
+						<div class="response" id="mce-error-response" style="display:none"></div>
+						<div class="response" id="mce-success-response" style="display:none"></div>
+					</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+				    <div style="position: absolute; left: -5000px;" aria-hidden="true">
+				    	<input type="text" name="b_37ab6470efbc61209ca1850ed_c897d012ba" tabindex="-1" value="">
+				    </div>
+				    <div class="clear">
+				    	<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+				    </div>
+			    </div>
+			</form>
+		</div>
+
+	<!--End mc_embed_signup-->
+		<p>BY TECH FOR KOREA</p>
+	</div>
+		
+</section>
+
+
+<?php get_footer(); ?>
