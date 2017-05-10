3a167e3ceeaf0a673331c0e0030f217590a0fac5
sean-cpw <sean@cmykpluswhite.com>
2017-05-10 00:28:09 -0400

    Create new file - career page

---
+++ themes/throne-child/career.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Template Name: Career Page
+ */
+?>
+<?php get_header(); ?>
+
+<script type="text/javascript">
+	window.location = "https://docs.google.com/forms/d/e/1FAIpQLScHrPAHn6dkfunnndZeeV_FkTNMsw2C06OJGD6qOz3mTrBicw/viewform?c=0&w=1";
+</script>
+
+<section id="main" class="content_wrapper">
+	<div id="tfk_company_db_header">
+		<a href="https://docs.google.com/forms/d/e/1FAIpQLScHrPAHn6dkfunnndZeeV_FkTNMsw2C06OJGD6qOz3mTrBicw/viewform?c=0&w=1">
+			<h1>CLICK HERE to APPLY</h1>
+		</a>
+	</div>
+</section>
+
+
+<?php get_footer(); ?>
